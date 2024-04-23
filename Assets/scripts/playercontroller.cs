bb310b4c71aacf91a5b9ced1ef5435897df78968
Alex235064 <116053389+Alex235064@users.noreply.github.com>
2024-04-23 17:46:26 +0700

    alexkalamisversion2plaercontroller

---
+++ Assets/scripts/playercontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if (moveX != 0)
+        {
+            moveY = 0;
+        }
+
+        Vector3 movement = new Vector3(moveX, moveY, 0);
+        transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+}
